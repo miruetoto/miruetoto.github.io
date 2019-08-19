cbd45231999035dc7a7faee3183fe8daddcf1c6e
miruetoto <44726070+miruetoto@users.noreply.github.com>
2019-08-20 08:54:02 +0900

    Update math.py

--- code/math.py
+++ code/math.py
@@ -1,3 +1,8 @@
+def cor(a,b):
+    a=np.asmatrix(np.array(a)).T;b=np.asmatrix(np.array(b)).T; 
+    rtn=a.T*b/np.sqrt(a.T*a)/np.sqrt(b.T*b)
+    return rtn[0,0]
+
 def grd(f,x):
     h=1e-4
     grad=np.zeros_like(x)
@@ -11,3 +16,4 @@ def grd(f,x):
         grad[idx]=(fxh1-fxh2)/(2*h)
         x[idx]=tmp_val 
     return grad   
+
