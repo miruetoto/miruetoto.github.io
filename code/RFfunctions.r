e6ae75b49b0e064d5477335ec6e4c15f4ac371a1
miruetoto <44726070+miruetoto@users.noreply.github.com>
2019-11-14 19:10:48 +0900

    Update RFfunctions.r

--- code/RFfunctions.r
+++ code/RFfunctions.r
@@ -1,5 +1,5 @@
 ## load pckg
 listofpackages <- c("ADMM")
 newpackages <- listofpackages[!(listofpackages %in% installed.packages()[,"Package"])]
-if(length(newpackages)) install.packages(newpackages)
+if(length(newpackages)) install.packages(newpackages,repos='http://cran.us.r-project.org')
 library(listofpackages)
