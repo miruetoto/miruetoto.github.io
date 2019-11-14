de30e74d7fd03c16d29438526983333428ac0153
miruetoto <44726070+miruetoto@users.noreply.github.com>
2019-11-14 19:07:28 +0900

    Update functions4RF.r

--- code/functions4RF.r
+++ code/functions4RF.r
@@ -1,4 +1,7 @@
-library(ADMM)
-sq4<-function(a){
-    a^2+1
-}
+## load pckg
+listofpackages <- c("ADMM")
+# ggforce : 4 geom_circle function
+# ggrepel : 4 geom_text_repel
+newpackages <- listofpackages[!(listofpackages %in% installed.packages()[,"Package"])]
+if(length(newpackages)) install.packages(newpackages)
+library(listofpackages)
