90d652a8df8008be9dded834525a102b262e7f80
miruetoto <44726070+miruetoto@users.noreply.github.com>
2019-11-14 19:08:08 +0900

    Update and rename functions4RF.r to RFfunctions.r

--- code/RFfunctions.r
+++ code/RFfunctions.r
@@ -1,7 +1,5 @@
 ## load pckg
 listofpackages <- c("ADMM")
-# ggforce : 4 geom_circle function
-# ggrepel : 4 geom_text_repel
 newpackages <- listofpackages[!(listofpackages %in% installed.packages()[,"Package"])]
 if(length(newpackages)) install.packages(newpackages)
 library(listofpackages)
