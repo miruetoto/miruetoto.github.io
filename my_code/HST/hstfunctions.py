6c65ff0261ecb3389632aad4f22c9d025927a0e3
miruetoto <44726070+miruetoto@users.noreply.github.com>
2019-12-25 10:40:27 +0900

    Update hstfunctions.py

--- my_code/HST/hstfunctions.py
+++ my_code/HST/hstfunctions.py
@@ -264,7 +264,7 @@ def pca4msvis2d(hstresult,τlist,
     M=len(τlist)
     dhh=np.asmatrix(hstresult[sprod('h',cc(0,τlist[0]))])
     sdistrslt0=snowdist(dhh)
-    pca4vis(sdistrslt0,nodename=nodename,groupindex=groupindex,figname=figname+str(1),figsize=figsize,dpi=dpi,cex=cex,text=text,fade=fade)
+    pca4vis2d(sdistrslt0,nodename=nodename,groupindex=groupindex,figname=figname+str(1),dpi=dpi,cex=cex,text=text,fade=fade)
     if prnt==True: print('obtain snowdist')
     for m in co(1,M):
         if prnt==True: print('\r'+str(m),'/'+str(M),sep='',end='')
