11ec744d7d11e26330ec694bc78a4d6ad13f0db9
miruetoto <miruetoto@gmail.com>
2020-02-11 15:34:18 +0900

    Update plot.py

--- my_code/plot.py
+++ my_code/plot.py
@@ -1,7 +1,7 @@
 ## python 
 import matplotlib as mpl 
 import matplotlib.pyplot as plt 
-from matplotlib import cycler
+# from matplotlib import cycler
 # Ipython_default=plt.rcParams.copy() # save initial value 
 # plt.rcParams.update(Ipython_default) # load initial value 
 
@@ -11,30 +11,30 @@ def pp_dpi(dpi=72):
 pp=plt
 pp.dpi=pp_dpi
 
-## ggplot
-ro.r('library(ggplot2)')
-import rpy2.robjects.lib.ggplot2 as gg
-from rpy2.robjects.lib import grdevices
-from IPython.display import Image#,display
-def ggshow(gg, w=1,h=1,r=1):
-    with grdevices.render_to_bytesio(grdevices.png,
-                                     type="cairo-png",
-                                     width=2000*w,
-                                     height=1000*h,
-                                     res=450*r,
-                                     antialias="subpixel") as b:
-        ro.r("print")(gg)
-    data = b.getvalue()
-    ip_img = Image(data=data, format='png', embed=True)
-    return ip_img
+# ## ggplot
+# ro.r('library(ggplot2)')
+# import rpy2.robjects.lib.ggplot2 as gg
+# from rpy2.robjects.lib import grdevices
+# from IPython.display import Image#,display
+# def ggshow(gg, w=1,h=1,r=1):
+#     with grdevices.render_to_bytesio(grdevices.png,
+#                                      type="cairo-png",
+#                                      width=2000*w,
+#                                      height=1000*h,
+#                                      res=450*r,
+#                                      antialias="subpixel") as b:
+#         ro.r("print")(gg)
+#     data = b.getvalue()
+#     ip_img = Image(data=data, format='png', embed=True)
+#     return ip_img
 
-def ggplot(data):
-    if type(data) is rpy2.robjects.vectors.DataFrame:
-        rtn=gg.ggplot(data)
-    else: 
-        rtn=gg.ggplot(p2r(pd.DataFrame(data)))
-    return rtn
-gg.plot=ggplot
-gg.show=ggshow
+# def ggplot(data):
+#     if type(data) is rpy2.robjects.vectors.DataFrame:
+#         rtn=gg.ggplot(data)
+#     else: 
+#         rtn=gg.ggplot(p2r(pd.DataFrame(data)))
+#     return rtn
+# gg.plot=ggplot
+# gg.show=ggshow
 
 ## rrplot
