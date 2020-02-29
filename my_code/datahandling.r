f484f81e1d1a18d5a267dcfab47df870acbfce9d
miruetoto <44726070+miruetoto@users.noreply.github.com>
2020-02-29 17:06:22 +0900

    Update datahandling.r

--- my_code/datahandling.r
+++ my_code/datahandling.r
@@ -7,3 +7,8 @@ cleaningvname<-function(dfdata){
     names(dfdata)<-str_replace_all(names(dfdata),"[.]","_")
     dfdata
 }
+
+minmaxsacling<-function(vector,range=c(0,1)){
+	vectorshift<-vector-min(vector)+range[0]
+	vectorshift/max(vectorshift)*range[1]
+}
