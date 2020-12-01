8eeb8fd70ded3310547fe5db2abb494af617b414
miruetoto <44726070+miruetoto@users.noreply.github.com>
2020-12-01 17:26:44 +0900

    Update datahandling.r

--- source/datahandling.r
+++ source/datahandling.r
@@ -66,11 +66,11 @@ plt<-function(...){
   ggplot(...)+theme_bw()+theme(
          axis.title.x=element_blank(),
          axis.title.y=element_blank(),
-         axis.text.y=element_text(family="Times",face="bold.italic",colour="blue"),
+         axis.text.y=element_text(family="Times",face="bold.italic",colour="gray50"),
          axis.text.x=element_text(family="Times",face="bold.italic",colour="gray50"),
          plot.title=element_text(size=rel(1.5),lineheight=0.9,family="Times",face="bold.italic",colour="black"),
-         legend.title=element_text(face="italic",family="Times",colour="blue",size=14),
-         legend.text=element_text(face="italic",family="Times",colour="blue",size=10)
+         legend.title=element_text(face="italic",family="Times",colour="gray50",size=14),
+         legend.text=element_text(face="italic",family="Times",colour="gray50",size=10)
         )
 }
 
